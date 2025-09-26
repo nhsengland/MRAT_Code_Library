95df0e9ebe40d059906a3b2e99ea51d9a2c635f6
Sholyoak <157801769+Sholyoak@users.noreply.github.com>
2025-09-26 13:56:25 +0100

    Update Avoidable attendances and Primary Care Collective flags.sql
    Tidied up formatting

--- Avoidable attendances and Primary Care Collective flags.sql
+++ Avoidable attendances and Primary Care Collective flags.sql
@@ -58,6 +58,7 @@
 				ed.[EC_AttendanceCategory]  = '1' AND 
 				ed.[EC_Arrival_Mode_SNOMED_CT] in ('1048061000000105','1048071000000103') --Public transport/taxi, Walk in
 		THEN '1' ELSE '0' end as 'Avoidable_flag',
+		
 	--Avoidable dental attendance flag
 	case when EC_Department_Type = '01' AND --Type 1 only
 			    ed.EC_Discharge_Status_SNOMED_CT in ('1077021000000100','182992009','1066321000000107') and
@@ -66,23 +67,28 @@
 				ed.[EC_AttendanceCategory]  = '1' and 
 				ed.[EC_Arrival_Mode_SNOMED_CT] in ('1048061000000105','1048071000000103')
 	THEN '1' ELSE '0' end as 'Avoidable_dental_flag',
+		
 	--Low acuity flag
 	case when ed.EC_Acuity_SNOMED_CT in ('1077241000000103','1077251000000100') --low and standard
 		THEN '1' ELSE '0' end as 'Low_standard_acuity',
+		
 	--ARI Flag
 	case when  rd1.Sheet_name like '%diagnosis%' 
 				and rd1.Der_ECDS_Group1 like '%infectious%'
 				and rd1.ECDS_Group2 like '%Respiratory%'
 	THEN '1' ELSE '0' end as 'ARI_Flag',
+		
 	--Mental Health Flag
 	case when	 rcc.Der_ECDS_Group1 = 'Psychosocial / Behaviour change' 
 			or  rd1.ECDS_Group2 = 'Mental health'  --diagnosis 1 group only does nto look at other diagnosis
 			or  [EC_Injury_Intent_SNOMED_CT] = '276853009'  --self inflicted injury
 			or   ed.EC_Discharge_Status_SNOMED_CT  = '1077041000000107' --Streamed to mental health service'  
          then '1' Else '0' end as 'Mental_Health_Flag',
+		
 		 --Self Harm Flag
 	case when	[EC_Injury_Intent_SNOMED_CT] = '276853009'  --self inflicted injury
         then '1' Else '0' end as 'Self_Harm_Flag',
+		
  	--Primary Care Open hour flag
 	case when (cast (ED.ARRIVAL_TIME as time) between '09:00:00' and '18:00:00' ) and
 	         [Working_Day_Type] = 'Y' --none Weekend and Bank Holidays
@@ -100,23 +106,24 @@
 									'289610003','290085007','248062006','366979004','48694002','248020004','386661006','80394007','302866003','410379003',
 									'162214009','13791008','161152002','182888003','84387000') 
 			then '1' else '0' end as 'Potential_Primary_Care_Complaint',
+		
     --Wound Care Flag
 	 CASE 
             WHEN rcc.snomed_code IN ('399963005', '299972003', '283682007') 
             THEN '1' ELSE '0' 
         END AS 'Wound_Care',
-  --Frailty Flag
-  CASE WHEN rcc.[ECDS_Description] IN (
-    'Dizziness', 
-    'Falls', 
-    'Falls / unsteady on feet', 
-    'Syncope (collapse with LOC)', 
-    'Near syncope (feeling faint without LOC)', 
-    'Unsteady when walking'
-  ) AND  ED.AGE_AT_ARRIVAL >= 75
-  THEN '1'
+  --Frailty Flag (not complete)
+  --CASE WHEN rcc.[ECDS_Description] IN (
+   -- 'Dizziness', 
+  --  'Falls', 
+  --  'Falls / unsteady on feet', 
+   -- 'Syncope (collapse with LOC)', 
+   -- 'Near syncope (feeling faint without LOC)', 
+   -- 'Unsteady when walking'
+--  ) AND  ED.AGE_AT_ARRIVAL >= 75
+ -- THEN '1'
   --WHEN [ECDS_Description] IS NULL THEN 'NULL'
-  ELSE '0'
+--  ELSE '0'
 --   case when  ED.AGE_AT_ARRIVAL >= 65
 --  AND (rD1.SNOMED_Code IN (
 --        '444814009', -- Frailty syndrome - this is the code for viral sinnitus
@@ -130,17 +137,20 @@
 --        '59282003'  -- Difficulty walking  falls cheif complaint = 161898004. use this??
 --      ))
 -- 	 then '1' else '0' 
-	 End AS 'Frailty_Flag',	 
+	-- End AS 'Frailty_Flag',	 
 
-	--Admitted/Non admitted
+	--Admitted/Non admitted -- Might not be in line with National Definition
 	rDD.der_ECDS_Group1 as 'Discharge group',
+		
 	--Age
 	CASE WHEN ED.AGE_AT_ARRIVAL <= 24 AND ED.AGE_AT_ARRIVAL >= 0 THEN '00 - 24'  
 	WHEN ED.AGE_AT_ARRIVAL <= 64 AND ED.AGE_AT_ARRIVAL >= 25 THEN '25 - 64' 
 	WHEN ED.AGE_AT_ARRIVAL >= 65 THEN '65+' 
 	ELSE 'Not Plausible' END AS 'AGE GROUP',
+		
 	--Breach 4 hour flag
 	case when ed.Der_EC_Duration > 241 then '1' else '0' end as 'Breach_4_hours',
+		
 	--Time/Date stamp issue
 	case when ed.Der_EC_Arrival_Date_Time is null then 'NULL_Arrival_Date/time'
 		when [EC_Initial_Assessment_Date] is null then 'NULL_Assessment_Date/time'
@@ -151,8 +161,8 @@
 		when cast([EC_Initial_Assessment_Date] + [EC_Initial_Assessment_Time] as datetime2) > cast([EC_Seen_For_Treatment_Date] +[EC_Seen_For_Treatment_Time] as DATETIME2) then 'Assessed after Treatment'
 		when cast([EC_Seen_For_Treatment_Date] +[EC_Seen_For_Treatment_Time] as datetime2) > [Clinically_Ready_To_Proceed_Timestamp] then 'Treated after discharge'
 		when [Clinically_Ready_To_Proceed_Timestamp] > [Der_EC_Departure_Date_Time] then 'Clinicallly Ready after discharge'
-	
-	else '?' end as 'DQ_Times',
+		else '?' end as 'DQ_Times',
+		
 	--National dashbaord flag
 	case when ([EC_Initial_Assessment_Time_Since_Arrival] >=0 OR [EC_Initial_Assessment_Time_Since_Arrival] IS NULL)
 AND  ([EC_Departure_Time_Since_Arrival]-[EC_Seen_For_Treatment_Time_Since_Arrival] >=0 OR [EC_Departure_Time_Since_Arrival]-[EC_Seen_For_Treatment_Time_Since_Arrival] IS NULL)
@@ -162,14 +172,18 @@ AND  ([EC_Departure_Time_Since_Arrival]-[Clinically_Ready_To_Proceed_Time_Since_
 AND  ([EC_Departure_Time_Since_Arrival] >=0 OR [EC_Departure_Time_Since_Arrival] IS NULL)
 AND  ([EC_Seen_For_Treatment_Time_Since_Arrival] >=0 OR [EC_Seen_For_Treatment_Time_Since_Arrival] IS NULL) 
 then '1' else '0' end as 'NAtional_Dashbaord_Flag',
+		
 	--Provider 
 	vophp.Integrated_Care_Board_Name as 'Provider_ICB',
 	vOPHP.Organisation_Name as 'Provider_Organisation_Name',
-		--Purchaser
+		
+	--Purchaser
 	rescom.Integrated_Care_Board_Name as 'Purchaser(Patient)_ICB',
+		
 	--site
 	vOPHS.Site_Name,
 	ed.EC_Department_Type,
+		
 	--event
 	ed.EC_Ident,
 	EC_AttendanceCategory,
@@ -254,22 +268,20 @@ ed.Der_EC_Duration,
 	             
 			
 FROM   
-
---[Reporting_MESH_ECDS].[EC_Core] AS ED --Reporting table
---[MESH_FUAECDS].[FUA_EC] as ED--Slim table
-[MESH_ECDS].[EC_Core]as ED --view verion of Datalake table
+		
+[MESH_ECDS].[EC_Core] as ED --view verion of Datalake table
 --[Reporting_MESH_ECDS].[EC_Core_snapshot] as ED --monthly snapshot of reporting table
 		--look up Provider
-left join [Reporting_UKHD_ODS].[Provider_Hierarchies_ICB]  as vOPHP on ED.[der_provider_code] = vOPHP.organisation_code 
+left join [UKHD_ODS].[Provider_Hierarchies_ICB]  as vOPHP on ED.[der_provider_code] = vOPHP.organisation_code 
 --Look up Site
-left join [Reporting_UKHD_ODS].[Provider_site]  as vOPHS on ED.[Site_Code_of_Treatment] = vOPHS.Site_code
+left join [UKHD_ODS].[Provider_site]  as vOPHS on ED.[Site_Code_of_Treatment] = vOPHS.Site_code
 --Look up Purchaser
 left join [MESH_ECDS].[EC_2425_Der] der on der.EC_Ident = ed.EC_ident and der.[Der_Financial_Year] = ed.[Der_Financial_year]
-left join [Reporting_UKHD_ODS].[Commissioner_Hierarchies_ICB] as rescom on rescom.[Organisation_Code] = der.[Responsible_Purchaser_Code] 
+left join [UKHD_ODS].[Commissioner_Hierarchies_ICB] as rescom on rescom.[Organisation_Code] = der.[Responsible_Purchaser_Code] 
 --Look up PAtient ICB
 left join [MESH_ECDS].[EC_CCG] ccg on  ccg.EC_Ident = ed.EC_ident and ccg.[Der_Financial_Year] = ed.[Der_Financial_year]
 --look up GP
-left join [Reporting_UKHD_ODS].[GP_Hierarchies_All] as vOGP on ed.GP_Practice_Code = vOGP.GP_Code and [Rel_Active] = 'TRUE' --get latest record with active PCN
+left join [UKHD_ODS].[GP_Hierarchies_All] as vOGP on ed.GP_Practice_Code = vOGP.GP_Code and [Rel_Active] = 'TRUE' --get latest record with active PCN
 --Get more date info
 left join [Internal_Reference].[Date_Full] as rDF on ED.ARRIVAL_DATE = rdf.full_date  
 	--get SNOMED code descriptions using ETOS,  select correct sheet name (these may need updating when new verions of ETOS are released). New view in development to avoid Code deprecated issue
@@ -294,16 +306,8 @@ where
 cast ( ed.Der_EC_Arrival_Date_Time as date ) >= '2022-01-01' and
 	 vOPHP.Region_Code = 'Y60' and
    vOPHP.NHSE_Organisation_Type = 'Acute Trust' 
- -- and ed.deleted = 0 --if using FAU slim table
-  --and (ed.EC_Arrival_Mode_SNOMED_CT is not null and rAS.SNOMED_UK_Preferred_Term is null)
-  --and vophp.Integrated_Care_Board_Name = 'NHS NOTTINGHAM AND NOTTINGHAMSHIRE INTEGRATED CARE BOARD'
-  --and der_provider_code = 'RWE'
-  and  ED.EC_Ident = '2690818080'
- 
- --and ED.[Site_Code_of_Treatment] = 'NQTE4' 
 
-
-	)
+ 	)
 --summary from cte
 
 Select  Provider_ICB, 
@@ -311,7 +315,6 @@ Select  Provider_ICB,
 EC_Department_Type,
 Month_Year,
 [AGE GROUP],
---allows to select mulitple flags
 Avoidable_flag,
 Avoidable_dental_flag,
 Low_standard_acuity,
@@ -347,5 +350,6 @@ Wound_Care,
 Breach_4_hours,
 Frailty_Flag
 
---order by Provider_ICB, [Purchaser(Patient)_ICB], Month_Year,Avoidable_flag,Avoidable_dental_flag,Low_standard_acuity,ARI_Flag,Mental_health_Flag,Potential_Primary_Care_Complaint,Primary_Care_Open
+
+
 
